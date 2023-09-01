4110df11b15eb83f9d5720f6239f1261614dc1c9
deffsin <deffsin@gmail.com>
2023-09-01 23:58:06 +0300

    Initial Commit

---
+++ GymUp/GymUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymUpApp.swift
+//  GymUp
+//
+//  Created by Denis Sinitsa on 01.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct GymUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
