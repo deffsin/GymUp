dad71c8f8f5188ac2da577a7a187c6baa0469f92
deffsin <deffsin@gmail.com>
2023-09-06 11:58:50 +0300

    Modified functionality in the UserManager and modified SignInEmailViewModel

--- GymUp/Services/Firestore/UserManager.swift
+++ GymUp/Services/Firestore/UserManager.swift
@@ -9,10 +9,31 @@ import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-struct DBUser {
+struct DBUser: Codable {
     let userId: String
     let email: String?
     let dataCreated: Date?
+    let isPremium: Bool?
+    
+    init(auth: AuthDataResultModel) {
+        self.userId = auth.uid
+        self.email = auth.email
+        self.dataCreated = Date()
+        self.isPremium = false
+    }
+    
+    init(
+    userId: String,
+    email: String?,
+    dataCreated: Date?,
+    isPremium: Bool?
+    ) {
+        self.userId = userId
+        self.email = email
+        self.dataCreated = dataCreated
+        self.isPremium = isPremium
+    }
+    
 }
 
 final class UserManager {
@@ -20,28 +41,58 @@ final class UserManager {
     static let shared = UserManager()
     private init() {}
     
-    func createNewUser(auth: AuthDataResultModel) async throws {
-        var userData: [String:Any] = [
-            "user_id": auth.uid,
-            "date_created": Timestamp()
-        ]
-        if let email = auth.email {
-            userData["email"] = email
-        }
-        
-        try await Firestore.firestore().collection("users").document(auth.uid).setData(userData, merge: false)
+    private let userCollection = Firestore.firestore().collection("users")
+    
+    private func userDocument(userId: String) -> DocumentReference {
+        userCollection.document(userId)
     }
     
+    private let encoder: Firestore.Encoder = { // encode перед созданием юзера и добавлением какого либо поля
+        let encoder = Firestore.Encoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        return encoder
+    }()
+    
+    private let decoder: Firestore.Decoder = { // decode при получении данных
+        let decoder = Firestore.Decoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+    
+    func createNewUser(user: DBUser) async throws {
+        try userDocument(userId: user.userId).setData(from: user, merge: false, encoder: encoder)
+    }
+    
+//    func createNewUser(auth: AuthDataResultModel) async throws {
+//        var userData: [String:Any] = [
+//            "user_id": auth.uid,
+//            "date_created": Timestamp()
+//        ]
+//        if let email = auth.email {
+//            userData["email"] = email
+//        }
+//
+//        try await userDocument(userId: auth.uid).setData(userData, merge: false)
+//    }
+    
     func getUser(userId: String) async throws -> DBUser {
-        let snapshot = try await Firestore.firestore().collection("users").document(userId).getDocument()
-        
-        guard let data = snapshot.data(), let userId = data["user_id"] as? String else {
-            throw URLError(.badServerResponse)
-        }
-        
-        let email = data["email"] as? String
-        let dataCreated = data["date_created"] as? Date
-        
-        return DBUser(userId: userId, email: email, dataCreated: dataCreated)
+        try await userDocument(userId: userId).getDocument(as: DBUser.self, decoder: decoder)
+    }
+    
+//    func getUser(userId: String) async throws -> DBUser {
+//        let snapshot = try await userDocument(userId: userId).getDocument()
+//
+//        guard let data = snapshot.data(), let userId = data["user_id"] as? String else {
+//            throw URLError(.badServerResponse)
+//        }
+//
+//        let email = data["email"] as? String
+//        let dataCreated = data["date_created"] as? Date
+//
+//        return DBUser(userId: userId, email: email, dataCreated: dataCreated)
+//    }
+    
+    func updateUserPremium(user: DBUser) async throws {
+        try userDocument(userId: user.userId).setData(from: user, merge: true, encoder: encoder)
     }
 }
