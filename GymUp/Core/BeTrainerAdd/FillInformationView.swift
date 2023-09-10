67513b814b3cf4e061f6e164c81e7b72fc28ef04
deffsin <deffsin@gmail.com>
2023-09-10 22:29:47 +0300

    Created ViewModel for FillInformationView and modified View

--- GymUp/Core/BeTrainerAdd/FillInformationView.swift
+++ GymUp/Core/BeTrainerAdd/FillInformationView.swift
@@ -7,21 +7,69 @@
 
 import SwiftUI
 
+@MainActor
+final class FillInformationViewModel: ObservableObject {
+    
+    @Published private(set) var user: DBUser? = nil
+    
+    @Published var fullname: String = ""
+    
+    func loadCurrentUser() async throws {
+        let authDataResult = try AuthenticationManager.shared.authenticatedUser()
+        self.user = try await UserManager.shared.getUser(userId: authDataResult.uid)
+    }
+    
+    func addTrainerFullname(fullname: String) {
+        Task {
+            let authDataResult = try AuthenticationManager.shared.authenticatedUser()
+            try? await UserManager.shared.addTrainerFullname(userId: authDataResult.uid, fullname: fullname)
+        }
+    }
+    
+}
+
 struct FillInformationView: View {
-    @State var name: String = ""
+    
+    @StateObject var viewModel = FillInformationViewModel()
+    
     @State var description: String = ""
     @State var gyms: [String] = []
     @State var phoneNumber: String = ""
+    @State var webLink: String = ""
     @State var instagram: String = ""
     @State var facebook: String = ""
     
     var body: some View {
         ZStack {
-            VStack {
-                TextField("Name and last name", text: $name)
-                    .padding()
-                    .frame(width: 190, height: 50)
-                    .
+            VStack(spacing: 15){
+                VStack(spacing: 10) { // your name, about you and etc
+                    TextField("Name and last name", text: $viewModel.fullname)
+                        .padding([.horizontal, .vertical], 5)
+                        .frame(width: 250, height: 40)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray.opacity(0.4), lineWidth: 1)
+                        }
+                    
+                    TextEditor(text: $description)
+                        .padding([.horizontal, .vertical], 5)
+                        .frame(width: 250, height: 100)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray.opacity(0.4), lineWidth: 1)
+                        }
+                }
+                
+                Button(action: {
+                    viewModel.addTrainerFullname(fullname: viewModel.fullname)
+                }) {
+                    Text("Add information")
+                        .frame(width: 130, height: 20)
+                        .padding()
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(15)
+                }
             }
         }
     }
