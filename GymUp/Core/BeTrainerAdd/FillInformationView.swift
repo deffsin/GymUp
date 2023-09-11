155d62aa0f69f56e2bd009a87788e6986f8eddcf
deffsin <deffsin@gmail.com>
2023-09-11 19:46:24 +0300

    Modified FillInformationView and ViewModel

--- GymUp/Core/BeTrainerAdd/FillInformationView.swift
+++ GymUp/Core/BeTrainerAdd/FillInformationView.swift
@@ -13,7 +13,9 @@ final class FillInformationViewModel: ObservableObject {
     @Published private(set) var user: DBUser? = nil
     
     // username?
+    // photo?
     @Published var fullname: String = ""
+    @Published var email: String = ""
     @Published var description: String = ""
     @Published var gyms: [String] = []
     @Published var phoneNumber: String = ""
@@ -27,102 +29,32 @@ final class FillInformationViewModel: ObservableObject {
         self.user = try await UserManager.shared.getUser(userId: authDataResult.uid)
     }
     
-    func addTrainerFullname(fullname: String) {
+    func addTrainerAllInformation(fullname: String, phoneNumber: String, email: String, description: String, webLink: String, instagram: String, facebook: String) {
         Task {
             let authDataResult = try AuthenticationManager.shared.authenticatedUser()
-            try? await UserManager.shared.addTrainerFullname(userId: authDataResult.uid, fullname: fullname)
+            try? await UserManager.shared.addTrainerAllInformation(userId: authDataResult.uid, fullname: fullname, phoneNumber: phoneNumber, email: email, description: description, webLink: webLink, instagram: instagram, facebook: facebook)
         }
     }
     
 }
 
 struct FillInformationView: View {
-    
+
     @Environment(\.dismiss) var dismiss
     @StateObject var viewModel = FillInformationViewModel()
-    
+
     var body: some View {
         ZStack {
-            VStack(spacing: 15){
-                VStack(spacing: 10) { // your name, about you and etc
-                    HStack {
-                        Text("Fullname:")
-                            .font(.system(size: 19))
-                        TextField("Name and last name", text: $viewModel.fullname)
-                            .padding([.horizontal, .vertical], 5)
-                            .frame(width: 250, height: 30)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.gray.opacity(0.4), lineWidth: 1)
-                            }
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        Text("Number:")
-                            .font(.system(size: 19))
-                        TextField("Phone number", text: $viewModel.phoneNumber)
-                            .padding([.horizontal, .vertical], 5)
-                            .frame(width: 250, height: 30)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.gray.opacity(0.4), lineWidth: 1)
-                            }
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        Text("Gyms:")
-                            .font(.system(size: 19))
-                        TextField("Gyms", text: $viewModel.none)
-                            .padding([.horizontal, .vertical], 5)
-                            .frame(width: 250, height: 30)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.gray.opacity(0.4), lineWidth: 1)
-                            }
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        Text("Link:")
-                            .font(.system(size: 19))
-                        TextField("Web link", text: $viewModel.webLink)
-                            .padding([.horizontal, .vertical], 5)
-                            .frame(width: 250, height: 30)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.gray.opacity(0.4), lineWidth: 1)
-                            }
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        Text("Instagram:")
-                            .font(.system(size: 19))
-                        TextField("Instagram", text: $viewModel.instagram)
-                            .padding([.horizontal, .vertical], 5)
-                            .frame(width: 250, height: 30)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.gray.opacity(0.4), lineWidth: 1)
-                            }
-                        Spacer()
-                    }
-                    
-                    HStack {
-                        Text("Facebook:")
-                            .font(.system(size: 19))
-                        TextField("Facebook", text: $viewModel.facebook)
-                            .padding([.horizontal, .vertical], 5)
-                            .frame(width: 250, height: 30)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.gray.opacity(0.4), lineWidth: 1)
-                            }
-                        Spacer()
-                    }
-                    
+            VStack(spacing: 15) {
+                InformationField(title: "Fullname:", placeholder: "Name and last name", text: $viewModel.fullname)
+                InformationField(title: "Email:", placeholder: "Email", text: $viewModel.email, keyboardType: .emailAddress)
+                InformationField(title: "Number:", placeholder: "Phone number", text: $viewModel.phoneNumber, keyboardType: .phonePad)
+                InformationField(title: "Gyms:", placeholder: "Gyms", text: $viewModel.none)
+                InformationField(title: "Link:", placeholder: "Web link", text: $viewModel.webLink)
+                InformationField(title: "Instagram:", placeholder: "Instagram", text: $viewModel.instagram)
+                InformationField(title: "Facebook:", placeholder: "Facebook", text: $viewModel.facebook)
+
+                VStack {
                     HStack {
                         Text("About you:")
                             .font(.system(size: 19))
@@ -137,9 +69,9 @@ struct FillInformationView: View {
                         Spacer()
                     }
                 }
-                
+
                 Button(action: {
-                    viewModel.addTrainerFullname(fullname: viewModel.fullname)
+                    viewModel.addTrainerAllInformation(fullname: viewModel.fullname, phoneNumber: viewModel.phoneNumber, email: viewModel.email, description: viewModel.description, webLink: viewModel.webLink, instagram: viewModel.instagram, facebook: viewModel.facebook)
                     dismiss()
                 }) {
                     Text("Add information")
@@ -155,8 +87,26 @@ struct FillInformationView: View {
     }
 }
 
-struct FillInformationView_Previews: PreviewProvider {
-    static var previews: some View {
-        FillInformationView()
+struct InformationField: View {
+    
+    var title: String
+    var placeholder: String
+    @Binding var text: String
+    var keyboardType: UIKeyboardType = .default
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.system(size: 19))
+            TextField(placeholder, text: $text)
+                .padding([.horizontal, .vertical], 5)
+                .frame(width: 250, height: 30)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.gray.opacity(0.4), lineWidth: 1)
+                }
+                .keyboardType(keyboardType)
+            Spacer()
+        }
     }
 }
