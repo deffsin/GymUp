09d477f56052c8e3fe44c5ef2006d7a2e378d0a7
deffsin <deffsin@gmail.com>
2023-09-15 16:31:01 +0300

    Separated various parts of the UI and one change in TabBarView

--- GymUp/Core/BeTrainerAdd/TrainerView.swift
+++ GymUp/Core/BeTrainerAdd/TrainerView.swift
@@ -10,102 +10,19 @@ import SwiftUI
 struct TrainerView: View {
     @StateObject var viewModel = TrainerViewModel()
     
-    
     var body: some View {
         NavigationStack {
             ZStack {
-                Color.black.opacity(0.7)
+                LinearGradient(colors: [.black.opacity(0.9), .mint.opacity(0.3)], startPoint: .top, endPoint: .bottom)
                     .ignoresSafeArea()
+                
                 ScrollView {
-                    ZStack {
-                        VStack {
-                            VStack(spacing: 25){
-                                Spacer()
-                                // if let trainer = viewModel.trainer {
-                                // Text(trainer.fullname ?? "")
-                                // Text(trainer.description ?? "")
-                                // }
-                                VStack(spacing: 15){
-                                    Image("image_male") // вокруг аватарки цвет какой то
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill) // scaledToFit?
-                                        .frame(width: 130, height: 130)
-                                        .clipShape(Circle())
-                                    
-                                    VStack(spacing: 5){
-                                        Text("Fullname")
-                                            .bold()
-                                        
-                                        Text("Location")
-                                            .font(.system(size: 17))
-                                        Text("Trainer at: Gym, MyFitness")
-                                    }
-                                    
-                                    HStack(spacing:25){
-                                        Button(action: {
-                                            if let url = URL(string: "https://instagram.com/deffsin") {
-                                                UIApplication.shared.open(url)
-                                            }
-                                        }) {
-                                            Image("instagram")
-                                                .resizable()
-                                                .frame(width: 35, height: 35)
-                                        }
-                                        
-                                        Button(action: {
-                                            if let url = URL(string: "https://facebook.com/") {
-                                                UIApplication.shared.open(url)
-                                            }
-                                        }) {
-                                            Image("facebook")
-                                                .resizable()
-                                                .frame(width: 35, height: 35)
-                                        }
-                                        
-                                        Button(action: {
-                                            if let url = URL(string: "https://linkedin.com/in/deffsin") {
-                                                UIApplication.shared.open(url)
-                                            }
-                                        }) {
-                                            Image("linkedin")
-                                                .resizable()
-                                                .frame(width: 35, height: 35)
-                                        }
-                                        
-                                        Button(action: {
-                                            if let url = URL(string: "https://google.com/") {
-                                                UIApplication.shared.open(url)
-                                            }
-                                        }) {
-                                            Image("web")
-                                                .resizable()
-                                                .frame(width: 35, height: 35)
-                                        }
-                                        
-                                    }
-                                    .frame(width: 240, height: 50)
-                                    .padding([.horizontal, .vertical], 5)
-                                    .foregroundColor(Color.white)
-                                    .background(Color.pink)
-                                    .cornerRadius(15)
-                                }
-                                
-                                VStack {
-                                    Text("aaa")
-                                }
-                                .frame(maxWidth: .infinity, minHeight: 350)
-                                .background(Color.blue)
-                                .cornerRadius(15)
-                                
-                                VStack {
-                                  // nothing
-                                }
-                                .frame(maxWidth: .infinity, minHeight: 40)
-                            }
-                            .foregroundColor(Color.white)
-                        }
-                        .padding([.leading, .trailing], 20)
+                    VStack(spacing: 25) {
+                        UserInfoView()
+                        DetailsView()
                     }
+                    .padding(.horizontal, 20)
+                    .foregroundColor(Color.white)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                 }
             }
@@ -113,7 +30,6 @@ struct TrainerView: View {
                 try? await Task.sleep(nanoseconds: 1_200_000_000)
                 try? await viewModel.loadCurrentUser()
             }
-
             .task {
                 try? await viewModel.loadCurrentUser()
             }
@@ -122,7 +38,155 @@ struct TrainerView: View {
             ToolbarItem(placement: .navigationBarLeading) {
                 Text("My profile")
                     .font(.system(size: 24))
+                    .foregroundColor(Color.white)
             }
+        }
+    }
+}
+
+struct UserInfoView: View {
+    @StateObject var viewModel = TrainerViewModel()
+    
+    var body: some View {
+        VStack(spacing: 15) {
+            Image("me")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 130)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+            if let trainer = viewModel.trainer {
+                VStack(spacing: 5){
+                    Text(trainer.fullname ?? "None")
+                        .bold()
+                    Text("Tallinn, Estonia")
+                        .font(.system(size: 17))
+                    Text("Trainer at: Gym, MyFitness")
+                }
+                
+                HStack(spacing:25) {
+                    socialButton(urlString: "\(trainer.instagram)", imageName: "instagram")
+                    socialButton(urlString: "\(trainer.facebook)", imageName: "facebook")
+                    socialButton(urlString: "https://linkedin.com/in/deffsin", imageName: "linkedin") // i don't have linkedin
+                    socialButton(urlString: "\(trainer.webLink)", imageName: "web")
+                        .padding(.trailing, 3)
+                }
+                .padding(8)
+                .foregroundColor(Color.white)
+                .background(Color.pink)
+                .cornerRadius(15)
+            }
+        }
+        .task {
+            try? await viewModel.loadCurrentUser()
+        }
+    }
+    
+    func socialButton(urlString: String, imageName: String) -> some View {
+        Button(action: {
+            if let url = URL(string: urlString) {
+                UIApplication.shared.open(url)
+            }
+        }) {
+            Image(imageName)
+                .resizable()
+                .frame(width: 35, height: 35)
+        }
+    }
+
+}
+
+struct DetailsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                RatingsView()
+                Divider()
+                RatingsInfo()
+            }
+            .frame(width: 300, height: 50)
+            Divider()
+            
+            AboutMeView()
+            Divider()
+            ResourcesView()
+        }
+        .frame(maxWidth: .infinity, minHeight: 200)
+        .padding(.bottom, 10)
+        .background(Color.white)
+        .foregroundColor(Color.black)
+        .cornerRadius(15)
+    }
+}
+
+struct RatingsView: View {
+    var body: some View {
+        Button(action: {}) {
+            HStack {
+                Text("Comments: ")
+                    .font(.system(size: 17))
+                Text("0")
+                    .bold()
+            }
+            .padding()
+        }
+    }
+}
+
+struct RatingsInfo: View {
+    var body: some View {
+        HStack(spacing: 2) {
+            Text("Rating: ")
+                .font(.system(size: 17))
+            Text("4.7")
+                .bold()
+            Image(systemName: "star.fill")
+                .foregroundColor(Color.yellow)
+        }
+        .padding()
+    }
+}
+
+struct AboutMeView: View {
+    @StateObject var viewModel = TrainerViewModel()
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            if let trainer = viewModel.trainer {
+                Text("About me:")
+                    .bold()
+                Text(trainer.description ?? "")
+                Spacer()
+            }
+        }
+        .padding(.horizontal, 10)
+        .task {
+            try? await viewModel.loadCurrentUser()
+        }
+    }
+}
+
+struct ResourcesView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text("Fitness resources:")
+                .bold()
+            ResourceButton(title: "Trainer programs")
+            Divider()
+            ResourceButton(title: "Nutrition for muscle gain")
+            Divider()
+            ResourceButton(title: "Nutrition for weight loss")
+        }
+        .padding(.horizontal, 10)
+    }
+    
+    func ResourceButton(title: String) -> some View {
+        HStack {
+            Button(action: {}) {
+                Text(title)
+                    .foregroundColor(Color.blue)
+            }
+            Spacer()
         }
     }
 }
