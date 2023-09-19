3e02a858ae6fb3358d87e2cf164263a912a5f61d
deffsin <deffsin@gmail.com>
2023-09-20 00:04:32 +0300

    Implemented func loadCurrentUser in the MainViewModel

--- GymUp/Core/Main/MainViewModel.swift
+++ GymUp/Core/Main/MainViewModel.swift
@@ -10,5 +10,29 @@ import SwiftUI
 
 @MainActor
 final class MainViewModel: ObservableObject {
+    
+    @Published private(set) var user: DBUser? = nil
+    @Published private(set) var trainer: TrainerInformation? = nil
         
+    func loadCurrentUser() async throws {
+        do {
+            let authDataResult = try AuthenticationManager.shared.authenticatedUser()
+            
+            guard let user = try? await UserManager.shared.getUser(userId: authDataResult.uid) else {
+                throw BeTrainerAddError.userRetrievalError
+            }
+            self.user = user
+            // print(BeTrainerAddError.userDataLoaded.localizedDescription)
+            
+            guard let trainerInfo = try? await UserManager.shared.getFirstTrainerInformation(userId: authDataResult.uid) else {
+                throw BeTrainerAddError.trainerRetrievalError
+            }
+            self.trainer = trainerInfo
+            // print(BeTrainerAddError.trainerDataLoaded.localizedDescription)
+            
+        } catch {
+            // An authentication issue
+            throw BeTrainerAddError.authenticationError
+        }
+    }
 }
