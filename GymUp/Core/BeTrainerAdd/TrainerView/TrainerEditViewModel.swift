c53c6b07ff876b8056599068a1ee7bf1c14bf294
deffsin <deffsin@gmail.com>
2023-09-27 11:55:26 +0300

    loadCurrentTrainer in the TrainerEditViewModel now using with Combine

--- GymUp/Core/BeTrainerAdd/TrainerView/TrainerEditViewModel.swift
+++ GymUp/Core/BeTrainerAdd/TrainerView/TrainerEditViewModel.swift
@@ -17,27 +17,64 @@ final class TrainerEditViewModel: ObservableObject {
     @Published var fullnameEdit: String = ""
     @Published var locationEdit: String = ""
     
-    func loadCurrentTrainer() async throws {
-        do {
-            let authDataResult = try AuthenticationManager.shared.authenticatedUser()
-            
-            guard let trainerInfo = try? await UserManager.shared.getFirstTrainerInformation(userId: authDataResult.uid) else {
-                throw BeTrainerAddError.trainerRetrievalError
+    private var cancellables = Set<AnyCancellable>()
+    
+    func loadCurrentTrainer() {
+        Future<TrainerInformation, Error> { promise in
+            Task {
+                do {
+                    let authDataResult = try AuthenticationManager.shared.authenticatedUser()
+                    guard let trainerInfo = try? await UserManager.shared.getFirstTrainerInformation(userId: authDataResult.uid) else {
+                        promise(.failure(BeTrainerAddError.trainerRetrievalError))
+                        return
+                    }
+                    promise(.success(trainerInfo))
+                } catch {
+                    promise(.failure(error))
+                }
             }
-            self.trainer = trainerInfo
-            
-            try? await getInformationForEdit()
-            
-            
-            print(BeTrainerAddError.trainerDataLoaded.localizedDescription)
-        } catch {
-            // An authentication issue
-            throw BeTrainerAddError.authenticationError
         }
+        .receive(on: DispatchQueue.main)
+        .sink(receiveCompletion: { completion in
+            switch completion {
+            case .finished:
+                print(BeTrainerAddError.trainerDataLoaded.localizedDescription)
+            case .failure(let error):
+                if let error = error as? BeTrainerAddError, error == .authenticationError {
+                    print("Authentication error")
+                } else {
+                    print("Error loading trainer info: \(error.localizedDescription)")
+                }
+            }
+        }, receiveValue: { [weak self] trainerInfo in
+            self?.trainer = trainerInfo
+            self?.getInformationForEdit()
+        })
+        .store(in: &cancellables)
     }
     
-    func getInformationForEdit() async throws {
+    func getInformationForEdit() {
         self.fullnameEdit = trainer?.fullname ?? ""
         self.locationEdit = trainer?.location ?? ""
     }
 }
+
+
+// func loadCurrentTrainer() async throws {
+//     do {
+//         let authDataResult = try AuthenticationManager.shared.authenticatedUser()
+//
+//         guard let trainerInfo = try? await // UserManager.shared.getFirstTrainerInformation(userId: authDataResult.uid) else {
+//             throw BeTrainerAddError.trainerRetrievalError
+//         }
+//         self.trainer = trainerInfo
+//
+//         try? await getInformationForEdit()
+//
+//
+//         print(BeTrainerAddError.trainerDataLoaded.localizedDescription)
+//     } catch {
+//         // An authentication issue
+//         throw BeTrainerAddError.authenticationError
+//     }
+// }
