8e9cd3324972dc870bf5548f86a308553fdec77a
deffsin <deffsin@gmail.com>
2023-10-06 23:40:26 +0300

    Added UserReviewsView

---
+++ GymUp/Core/Main/UserProfileCellDetail/UserReviews/UserReviewsView.swift
@@ -0,0 +1,20 @@
+//
+//  UserReviewsView.swift
+//  GymUp
+//
+//  Created by Denis Sinitsa on 06.10.2023.
+//
+
+import SwiftUI
+
+struct UserReviewsView: View {
+    var body: some View {
+        Text("UserReviewsView")
+    }
+}
+
+struct UserReviewsView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserReviewsView()
+    }
+}
