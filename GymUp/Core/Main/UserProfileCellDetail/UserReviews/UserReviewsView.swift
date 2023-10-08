31197e3998a3773dc508b80b046e8aa0700ee682
deffsin <deffsin@gmail.com>
2023-10-08 10:19:07 +0300

    Some part of the UI

--- GymUp/Core/Main/UserProfileCellDetail/UserReviews/UserReviewsView.swift
+++ GymUp/Core/Main/UserProfileCellDetail/UserReviews/UserReviewsView.swift
@@ -8,16 +8,56 @@
 import SwiftUI
 
 struct UserReviewsView: View {
-    @StateObject var userReviewsVM = UserReviewsViewModel()
+    @StateObject var userReviewsVM = UserReviewsViewModel() // возможно стоит создавать StateObject не прямо тут
     var trainer: TrainerInformation
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
     
     var body: some View {
         ZStack {
-            VStack {
-                Text("UserReviewsView")
+            BackgroundUserProfileCellDetailView()
+            
+            ScrollView {
                 if let userReviews = userReviewsVM.allReviews {
                     ForEach(userReviews, id: \.id) { review in
-                        Text(review.description!)
+                        VStack {
+                            VStack {
+                                ZStack {
+                                    UserRatingView(userRating: review.rating!)
+
+                                    HStack(spacing: 7) {
+                                        Text(review.fullname!)
+                                            .bold()
+                                        
+                                        Spacer()
+                                        Text(dateFormatter.string(from: review.dataCreated!))
+                                            .font(.footnote)
+                                    }
+                                    .frame(width: 220, height: 30)
+                                    .background(Color.red.opacity(0.5))
+                                    .padding(.top, 5)
+                                }
+                                
+                                Divider()
+                                
+                                HStack {
+                                    Text(review.description!)
+                                    Spacer()
+                                }
+                                .frame(width: 220)
+                                .frame(minHeight: 20)
+                                .padding(.bottom, 5)
+                            }
+                            .frame(width: 240)
+                            .frame(minHeight: 80)
+                            .background(Color.green.opacity(0.5))
+                        }
+                        .padding(.horizontal, 20)
                     }
                 }
             }
