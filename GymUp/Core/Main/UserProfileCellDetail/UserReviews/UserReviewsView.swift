a662167af84c1298f45a70f31f6c467056ffb4aa
deffsin <deffsin@gmail.com>
2023-10-09 00:07:25 +0300

    Modified UI for UserReviewsView

--- GymUp/Core/Main/UserProfileCellDetail/UserReviews/UserReviewsView.swift
+++ GymUp/Core/Main/UserProfileCellDetail/UserReviews/UserReviewsView.swift
@@ -27,37 +27,43 @@ struct UserReviewsView: View {
                     ForEach(userReviews, id: \.id) { review in
                         VStack {
                             VStack {
-                                ZStack {
-                                    UserRatingView(userRating: review.rating!)
-
-                                    HStack(spacing: 7) {
-                                        Text(review.fullname!)
-                                            .bold()
-                                        
-                                        Spacer()
-                                        Text(dateFormatter.string(from: review.dataCreated!))
-                                            .font(.footnote)
-                                    }
-                                    .frame(width: 220, height: 30)
-                                    .background(Color.red.opacity(0.5))
-                                    .padding(.top, 5)
+                                HStack(spacing: 7) {
+                                    Text(review.fullname!)
+                                        .bold()
+                                    
+                                    Spacer()
+                                    Text(dateFormatter.string(from: review.dataCreated!))
+                                        .font(.footnote)
                                 }
+                                .frame(width: 235, height: 30)
+                                // .background(Color.red.opacity(0.5))
+                                .padding(.top, 5)
                                 
                                 Divider()
                                 
-                                HStack {
-                                    Text(review.description!)
-                                    Spacer()
+                                VStack(spacing: 5){
+                                    HStack {
+                                        Text(review.description!)
+                                        Spacer()
+                                    }
+                                    UserRatingView(userRating: review.rating!)
                                 }
-                                .frame(width: 220)
+                                .frame(width: 235)
                                 .frame(minHeight: 20)
-                                .padding(.bottom, 5)
+                                .padding(.bottom, 10)
                             }
-                            .frame(width: 240)
+                            .frame(width: 270)
                             .frame(minHeight: 80)
-                            .background(Color.green.opacity(0.5))
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(LinearGradient(gradient: Gradient(colors: [Color.pink, Color.mint]), startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 1)
+                            )
+                            .shadow(color: Color.black, radius: 3)
                         }
                         .padding(.horizontal, 20)
+                        .padding(.bottom, 20)
                     }
                 }
             }
