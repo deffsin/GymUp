a5088732764b44003fc8f312aa2bcbba0441ef23
deffsin <deffsin@gmail.com>
2023-10-12 22:50:56 +0300

    UserInfoView test commit after updating the Xcode

--- GymUp/Core/BeTrainerAdd/TrainerView/TrainerSubViews/UserInfoView.swift
+++ GymUp/Core/BeTrainerAdd/TrainerView/TrainerSubViews/UserInfoView.swift
@@ -21,46 +21,48 @@ struct UserInfoView: View {
                 .shadow(color: .white, radius: 2)
             
             if let trainer = viewModel.trainer {
-                VStack(spacing: 5){
-                    HStack(spacing: 5) {
-                        Image(systemName: "person.text.rectangle")
-                            .font(.system(size: 12))
+                if let trainerName = viewModel.user {
+                    VStack(spacing: 5){
+                        HStack(spacing: 5) {
+                            Image(systemName: "person.text.rectangle")
+                                .font(.system(size: 12))
+                            
+                            Text(trainerName.username ?? "")
+                                .bold()
+                                .font(.system(size: 19))
+                        }
                         
-                        Text(trainer.fullname ?? "")
-                            .bold()
-                            .font(.system(size: 19))
+                        HStack(spacing: 5) {
+                            Image(systemName: "location")
+                                .font(.system(size: 12))
+                            
+                            Text(trainer.location ?? "")
+                                .font(.system(size: 15))
+                        }
+                        .foregroundColor(Color.white.opacity(0.9))
+                        
+                        HStack(spacing: 5) {
+                            Image(systemName: "dumbbell")
+                                .font(.system(size: 12))
+                            
+                            Text("Trainer at: \(trainer.gyms ?? "")")
+                                .font(.system(size: 17))
+                        }
                     }
                     
-                    HStack(spacing: 5) {
-                        Image(systemName: "location")
-                            .font(.system(size: 12))
-                        
-                        Text(trainer.location ?? "")
-                            .font(.system(size: 15))
+                    HStack(spacing:25) {
+                        socialButton(urlString: "\(trainer.instagram ?? "")", imageName: "instagram")
+                        socialButton(urlString: "\(trainer.facebook ?? "")", imageName: "facebook")
+                        socialButton(urlString: "\(trainer.linkedIn ?? "")", imageName: "linkedin")
+                        socialButton(urlString: "\(trainer.webLink ?? "")", imageName: "web")
+                            .padding(.trailing, 3)
                     }
-                    .foregroundColor(Color.white.opacity(0.9))
-                    
-                    HStack(spacing: 5) {
-                        Image(systemName: "dumbbell")
-                            .font(.system(size: 12))
-                        
-                        Text("Trainer at: \(trainer.gyms ?? "")")
-                            .font(.system(size: 17))
-                    }
+                    .padding(8)
+                    .foregroundColor(Color.white)
+                    .background(Color.pink)
+                    .cornerRadius(15)
+                    .shadow(color: .pink, radius: 5)
                 }
-                
-                HStack(spacing:25) {
-                    socialButton(urlString: "\(trainer.instagram ?? "")", imageName: "instagram")
-                    socialButton(urlString: "\(trainer.facebook ?? "")", imageName: "facebook")
-                    socialButton(urlString: "\(trainer.linkedIn ?? "")", imageName: "linkedin")
-                    socialButton(urlString: "\(trainer.webLink ?? "")", imageName: "web")
-                        .padding(.trailing, 3)
-                }
-                .padding(8)
-                .foregroundColor(Color.white)
-                .background(Color.pink)
-                .cornerRadius(15)
-                .shadow(color: .pink, radius: 5)
             }
         }
     }
